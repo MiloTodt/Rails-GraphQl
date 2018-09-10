b8fe201638b9fe266a358e1e622d1457b59c465c
Milo Todt <Milo.Todt@gmail.com>
2018-09-09 21:47:25 -0700

    password check

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validate :strong_password
+
+  private
+  def strong_password
+
+    # At least one of each: Uppercase letter, lowercase, number.
+
+    strong_password = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8}/
+    has_strong_password = strong_password.match?(password)
+
+    unless has_strong_password
+      errors.add(:password, 'is not strong enough')
+    end
+  end
+
 end
